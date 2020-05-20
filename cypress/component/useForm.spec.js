42c091b250d9a139cf3cea78345da798392c0cc9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-19 22:16:17 -0400

    initial

---
+++ cypress/component/useForm.spec.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+import React from 'react'
+import { mount } from 'cypress'
+import { useForm } from 'react-hook-form'
+
+function App() {
+  const { register, handleSubmit, errors } = useForm(); // initialise the hook
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input name="firstname" ref={register} /> {/* register an input */}
+
+      <input name="lastname" ref={register({ required: true })} />
+      {errors.lastname && 'Last name is required.'}
+
+      <input name="age" ref={register({ pattern: /\d+/ })} />
+      {errors.age && 'Please enter number for age.'}
+
+      <input type="submit" />
+    </form>
+  );
+}
+
+describe('useForm', () => {
+  it('submits a form', () => {
+    mount(<App />)
+  })
+})
